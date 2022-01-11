a433f09744b99a49595b7d1aff96d1a7c360bb13
namtq184161 <quangnam260800@gmail.com>
2022-01-11 23:59:27 +0700

    add link

--- modules/banner/banner.php
+++ modules/banner/banner.php
@@ -1,9 +1,9 @@
 <div id="banner">
     <div class="banner-item">
-        <a href="#"><img class="img-fluid" src="images/banner-1.png"></a>
+        <a href="index.php?page_layout=category&cat_id=1&cat_name=iPhone"><img class="img-fluid" src="images/banner-1.png"></a>
     </div>
     <div class="banner-item">
-        <a href="#"><img class="img-fluid" src="images/banner-2.png"></a>
+        <a href="index.php?page_layout=product&prd_id=8"><img class="img-fluid" src="images/banner-2.png"></a>
     </div>
     <div class="banner-item">
         <a href="index.php?page_layout=product&prd_id=1"><img class="img-fluid" src="images/banner-3.png"></a>
